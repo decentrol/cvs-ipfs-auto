1dcab1108e3f2ae9872907872b78dc8108dc335e
douwefa <43823020+douwefa@users.noreply.github.com>
2018-11-03 00:42:58 +0100

    Update log.js

--- logging/log.js
+++ logging/log.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 
-let ipfs = require('ipfs-api')({host: "localhost", port: 5001, protocol: "http"});
+let ipfs = require('ipfs-api')({host: "localhost", port: 5001, protocol: "http"}); //require ipfs-api which can be installed using npm command line.
 
 
 setInterval(function(){
-    // your code goes here...
 
+//run this piece of code every 24 hours 
+// getting today's date with the following lines
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
@@ -20,13 +21,14 @@ if(mm<10) {
 }
 today = dd + '-' + mm + '-' + yyyy;
 
-let cont = fs.readFileSync('Temp_'+today+'.cvs');
-
+let cont = fs.readFileSync('Temp_'+today+'.cvs'); //the name of the cvs file that we want to push out to the ipfs network
+ 
+    
   cont = new Buffer(cont);
   ipfs.add(cont, function (err, hash) {
     if(err) throw err;
     console.log(hash);
 
   });
-
+//the hash is returned on the console log. I don't really get the piece of code above but it works, lol 
 }, 24 * 60 * 60 * 1000);
