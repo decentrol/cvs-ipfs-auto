76f511c0eff2ea213538ad5f2deff475e036e516
douwefa <43823020+douwefa@users.noreply.github.com>
2018-11-03 13:13:11 +0100

    Update log.js

--- logging/log.js
+++ logging/log.js
@@ -1,12 +1,25 @@
 const fs = require("fs");
 
-let ipfs = require('ipfs-api')({host: "localhost", port: 5001, protocol: "http"}); //require ipfs-api which can be installed using npm command line.
+let ipfs = require('ipfs-api')({host: "localhost", port: 5001, protocol: "http"});
 
+
+tDate = new Date();
+tDate.setHours(23);
+tDate.setMinutes(59);
+tDate.setSeconds(00);
+tDate.setMilliseconds(500);
+
+tMillis = tDate - new Date();
+
+if (tMillis < 0)
+  tMillis = tMillis + 24 * 60 * 60 * 1000; // if time is greater than 21:36:00:500 just add 24 hours as it will execute next day
+
+setTimeout(function(){
  
+
 setInterval(function(){
 
-//run this piece of code every 24 hours 
-// getting today's date with the following lines
+
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
@@ -21,14 +34,15 @@ if(mm<10) {
 }
 today = dd + '-' + mm + '-' + yyyy;
 
-let cont = fs.readFileSync('temp_'+today+'.cvs'); //the name of the cvs file that we want to push out to the ipfs network
- 
-    
+let cont = fs.readFileSync('loggings/temp_'+today+'.csv');
+
   cont = new Buffer(cont);
   ipfs.add(cont, function (err, hash) {
     if(err) throw err;
     console.log(hash);
 
   });
-//the hash is returned on the console log. I don't really get the piece of code above but it works, lol 
-}, 24 * 60 * 60 * 1000);
+
+},  24 * 60 * 60 * 1000); //run script every 24 hours 
+
+}, tMillis); //run script at 23:59:00
